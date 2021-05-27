a709ae6d958a8fe080e289ee3fe08407401e6808
Dana Sevilla <dana.sevilla@ghl.com>
2021-05-27 14:52:37 +0800

    2nd commit

--- app/src/main/java/com/drbs/bitsandpizzas/PastaFragment.java
+++ app/src/main/java/com/drbs/bitsandpizzas/PastaFragment.java
@@ -4,21 +4,51 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class PastaFragment extends ListFragment {
+public class PastaFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pasta));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView pastaRecycler = (RecyclerView) inflater.inflate(
+                R.layout.fragment_pasta, container, false);
+
+        String[] pastaNames = new String[Pasta.pastas.length];
+        for (int i = 0; i < Pasta.pastas.length; i++) {
+            pastaNames[i] = Pasta.pastas[i].getName();
+        }
+
+        int[] pastaImages = new int[Pasta.pastas.length];
+        for (int i = 0; i < Pasta.pastas.length; i++) {
+            pastaImages[i] = Pasta.pastas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pastaNames, pastaImages);
+        pastaRecycler.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pastaRecycler.setLayoutManager(layoutManager);
+
+        return pastaRecycler;
     }
 }
+
+//public class PastaFragment extends ListFragment {
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                inflater.getContext(),
+//                android.R.layout.simple_list_item_1,
+//                getResources().getStringArray(R.array.pasta));
+//        setListAdapter(adapter);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
+//}
